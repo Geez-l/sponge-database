682658aadf2d9b9588ee0e4b0801a6c0ed06c390
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2025-07-13 01:23:22 +0800

    added changes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // import './App.css'
 
-/*
 import {
     BrowserRouter as Router,
     Routes,
     Route,
     useLocation,
-
 } from 'react-router-dom';
-
-*/
 
 // import 'bootstrap-icons/font/bootstrap-icons.css';
 
 // MAIN PAGES
-
-import home from './components/home';
+import Home from './components/home';
 // import result from './components/result';
 
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(0);
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
 
-const App = () 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home/>} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
