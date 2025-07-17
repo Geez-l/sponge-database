9cc6f1b58a5b342e9a1b41a475a2e34c0b6806f0
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2025-07-18 05:37:20 +0800

    result changes
    changed search bar, navigate result details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,10 @@ import './css/variable.css';
 
 import { useSpongeFilters } from './hooks/useSpongeFilters';
 import { useRouter } from 'next/navigation';
-import { Button, FormControl } from 'react-bootstrap';
-import { FaSearch} from 'react-icons/fa';
+import { Button } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
 
+import HomeCards from './components/homeCards'; 
 
 
 /* HOME resources
@@ -67,192 +68,179 @@ const Home = () => {
         <Card className="sponge-card">
           <div className="search-container">
 
-            <form className="d-flex w-100 justify-content-center mb-3"
-             onSubmit={e => { e.preventDefault(); }}>
-            <FormControl
-            type='Search'
-            placeholder='Enter keywords'
-            className='searchKeyword'
-            aria-label='Search'
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-            />
-              {/* Button*/}
-            <Button 
-            type='submit'
-            className='search-icon'
+            {/* Search Form */}
+            <form
+              className="search-bar-wrapper"
+              onSubmit={e => {
+                e.preventDefault();
+                handleSubmitAndNavigate();
+              }}
             >
-              <FaSearch/>
-            </Button>
+              <div className="search-bar-left-icon">
+                <FaSearch className="search-icon" />
+                <input
+                  type="text"
+                  placeholder="Enter keyword"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="search-input"
+                />
+              </div>
             </form>
 
-            <div className='advancedSearch'>
+            {/* Advanced Search Label */}
+            <div className="advancedSearch">
               <h1>OR SEARCH BY</h1>
             </div>
 
-            {/* Color Dropdown */}
-            <Row className='w-100 mb-2'>
-              <Col md={4} className='mb-2'>
-            <Dropdown className="dropDown1">
-              <Dropdown.Toggle variant="success" id="dropdown-color">
-                {selectedColor}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {colors.map((color: string, index: number) => (
-                  <Dropdown.Item
-                    key={index}
-                    onClick={() => handleColorSelect(color)}
-                    active={selectedColor === color}
-                  >
-                    {color}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-            </Col>
-           
+            {/* First Row of Dropdowns */}
+            <Row className="w-100 mb-2">
+              <Col md={4} className="mb-2">
+                <Dropdown className="dropDown1">
+                  <Dropdown.Toggle variant="success" id="dropdown-color">
+                    {selectedColor}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {colors.map((color, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleColorSelect(color)}
+                        active={selectedColor === color}
+                      >
+                        {color}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
 
-            <Col md={4} className='mb-2'>
-            {/* Functional Form Dropdown */}
-            <Dropdown className="dropDown2">
-              <Dropdown.Toggle variant="success" id="dropdown-form">
-                {selectedFunctionalForm}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {functionalForms.map((form: string, index: number) => (
-                  <Dropdown.Item
-                    key={index}
-                    onClick={() => handleFunctionalFormSelect(form)}
-                    active={selectedFunctionalForm === form}
-                  >
-                    {form}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-            </Col>
+              <Col md={4} className="mb-2">
+                <Dropdown className="dropDown2">
+                  <Dropdown.Toggle variant="success" id="dropdown-form">
+                    {selectedFunctionalForm}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {functionalForms.map((form, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleFunctionalFormSelect(form)}
+                        active={selectedFunctionalForm === form}
+                      >
+                        {form}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
 
-            <Col md={4} className='mb-2'>
-            {/* Putative classification */}
-            <Dropdown className='dropDown3'>
-              <Dropdown.Toggle variant='success' id='dropdown-classification'>
-                {selectedClassification}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {classification.map((classification: string, index: number) => (
-                  <Dropdown.Item
-                  key={index}
-                  onClick={() => handleClassificationSelect(classification)}
-                  active={selectedClassification === classification}
-                  >
-                    {classification}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-            </Col>
+              <Col md={4} className="mb-2">
+                <Dropdown className="dropDown3">
+                  <Dropdown.Toggle variant="success" id="dropdown-classification">
+                    {selectedClassification}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {classification.map((item, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleClassificationSelect(item)}
+                        active={selectedClassification === item}
+                      >
+                        {item}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </Col>
             </Row>
 
-            <Row className='w-100 mb-4'>
-              <Col md={4} className='mb-2'>
-              <Dropdown className='dropDown4'>
-              <Dropdown.Toggle variant='success' id='dropdown-growth'>
-                {selectedGrowth}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {growth.map((growth: string, index: number) => (
-                  <Dropdown.Item
-                  key={index}
-                  onClick={() => handleGrowthSelect(growth)}
-                  active={selectedGrowth === growth}
-                  >
-                    {growth}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
+            {/* Second Row of Dropdowns */}
+            <Row className="w-100 mb-4">
+              <Col md={4} className="mb-2">
+                <Dropdown className="dropDown4">
+                  <Dropdown.Toggle variant="success" id="dropdown-growth">
+                    {selectedGrowth}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {growth.map((g, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleGrowthSelect(g)}
+                        active={selectedGrowth === g}
+                      >
+                        {g}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
               </Col>
 
-              <Col md={4} className='mb-2'>
-              <Dropdown className='dropDown5'>
-              <Dropdown.Toggle variant='success' id='dropdown-surface'>
-                {selectedSurface}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {surface.map((surface: string, index: number) => (
-                  <Dropdown.Item
-                  key={index}
-                  onClick={() => handleSurfaceSelect(surface)}
-                  active={selectedSurface === surface}
-                  >
-                    {surface}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
+              <Col md={4} className="mb-2">
+                <Dropdown className="dropDown5">
+                  <Dropdown.Toggle variant="success" id="dropdown-surface">
+                    {selectedSurface}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {surface.map((s, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleSurfaceSelect(s)}
+                        active={selectedSurface === s}
+                      >
+                        {s}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
               </Col>
 
-              <Col md={4} className='mb-2'>
-              <Dropdown className='dropDown6'>
-              <Dropdown.Toggle variant='success' id='dropdown-surface'>
-                {selectedLocation}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {location.map((location: string, index: number) => (
-                  <Dropdown.Item
-                  key={index}
-                  onClick={() => handleLocationSelect(location)}
-                  active={selectedLocation === location}
-                  >
-                    {location}
-                  </Dropdown.Item>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
+              <Col md={4} className="mb-2">
+                <Dropdown className="dropDown6">
+                  <Dropdown.Toggle variant="success" id="dropdown-location">
+                    {selectedLocation}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    {location.map((loc, index) => (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => handleLocationSelect(loc)}
+                        active={selectedLocation === loc}
+                      >
+                        {loc}
+                      </Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
               </Col>
-
             </Row>
 
+            {/* Submit and Reset Buttons */}
+            <Row className="submit-button">
+              <Col md={4} className="mb-2">
+                <div className="resetButton">
+                  <button className="btn btn-outline-danger" onClick={handleReset}>
+                    Reset
+                  </button>
+                </div>
+              </Col>
 
-         <Row className='submit-button'>
-          <Col md={4} className='mb-2'>
-             {/* Buttons */}
-             <div className="resetButton">
-              <button className="btn btn-outline-danger" onClick={handleReset}>
-                Reset
-              </button>
-            </div>
-          </Col>
-
-          <Col md={4} className='mb-2'>
-          <div className="submitButton">
-              <button className="btn btn-success" onClick={handleSubmitAndNavigate}>
-                Submit
-              </button>
-            </div>
-          </Col>
-         </Row>
+              <Col md={4} className="mb-2">
+                <div className="submitButton">
+                  <button className="btn btn-success" onClick={handleSubmitAndNavigate}>
+                    Submit
+                  </button>
+                </div>
+              </Col>
+            </Row>
           </div>
         </Card>
         <div className='banner-msg'>
           <h1>DIVE IN AND DISCOVER</h1>
           <h2> The gateway to the Philippine Marine Sponges</h2>
+          <h3>hello world</h3>
         </div>
-        </div>
-
-      </main>
-
-      <div className='home-bottom'>
-        <Card>
-          <div className='search-db-text'>
-            <h1> How to Search Our Database</h1>
-            <h3>text text text</h3>
-          </div>
-        </Card>
       </div>
-      {/* Add another card here */}
-      {/* Follow the logic in dropdowns, 1 row that encloses columns
-      row{col col col } */}
+      </main>
+      <HomeCards />
     </div>
     
   );
