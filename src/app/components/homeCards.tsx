28e26256bdc14672dc33a0120c56b7d7bad46191
ocsonelishaj <ocsonelishaj@gmail.com>
2025-07-30 13:59:01 +0800

    Update homeCards.tsx

--- src/app/components/homeCards.tsx
+++ src/app/components/homeCards.tsx
@@ -51,41 +51,23 @@ const HomeCards = () => (
       <Card.Body>
         <Card.Title className='card-heading'><span className='title-bar'>|</span> About Philippine Marine Sponges</Card.Title>
         <div className='card-subpart'>
-          <Card.Subtitle className='card-subheading'>Subheading 1</Card.Subtitle>
+          <Card.Subtitle className='card-subheading'>What are Sponges?</Card.Subtitle>
           <Card.Text className='card-body-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
+            Sponges are known to be simple invertebrate animals that reside in aquatic environments. They are categorized as simple yet remarkable organisms that play an important role in the marine ecosystem. Sponges belong to the phylum Porifera, which includes some of the most primitive multicellular animals on Earth. Unlike other marine species, sponges lack true tissues and organs as their bodies are specialized cells for life functions such as filter feeding, respiration, and reproduction (University of Hawaiʻi at Mānoa, n.d.). 
           </Card.Text>
         </div>
         
         <div className='card-subpart'>
-          <Card.Subtitle className='card-subheading'>Subheading 2</Card.Subtitle>
+          <Card.Subtitle className='card-subheading'>Why Sponges Matter?</Card.Subtitle>
           <Card.Text className='card-body-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
+            Sponges are known for filter feeding, which pumps water through their bodies, removing bacteria and pollutants from their environment. This function supports the health of coral reefs and other marine habitats (NOAA, 2024). Additionally, sponges provide shelter for various marine creatures, from algae to small invertebrates. By doing all this, sponges help keep the reef ecosystem balanced.
           </Card.Text>
         </div>
 
         <div className='card-subpart'>
-          <Card.Subtitle className='card-subheading'>Subheading 3</Card.Subtitle>
+          <Card.Subtitle className='card-subheading'>Why Philippine Sponge Guide?</Card.Subtitle>
           <Card.Text className='card-body-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
+            This database is more than just a collection of sponge records, but a step towards biodiversity conservation and citizen science. This database hopes to make marine biodiversity records more accessible and empower researchers, students, and local communities to take part in protecting the Philippine marine life. 
           </Card.Text>
         </div>
       </Card.Body>
@@ -102,7 +84,13 @@ const HomeCards = () => (
           Hooper, J. and Van Soest, R. (2002) Systema Porifera. A Guide to the Classification of Sponges. Springer, New York. http://dx.doi.org/10.1007/978-1-4615-0747-5_1
           <br />
           <br />
+          NOAA. (2024). What is a sponge? National Ocean Service. https://oceanservice.noaa.gov/facts/sponge.html
+          <br />
+          <br />
           Schonberg, C. 2021. No taxonomy needed: Sponge functional morphologies inform about environmental conditions. Ecological Indicators. 129. 107806. 10.1016/j.ecolind.2021.107806.
+          <br />
+          <br />
+          University of Hawaiʻi at Mānoa. (n.d.). Phylum Porifera. Exploring Our Fluid Earth. https://manoa.hawaii.edu/exploringourfluidearth/biological/invertebrates/phylum-porifera
         </Card.Text>
       </Card.Body>
     </Card>
