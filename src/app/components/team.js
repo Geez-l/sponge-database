fa0257632d4b8fb4d7f444289b417d446c4aa8e0
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2025-07-30 13:02:37 +0800

    updated the aboutus

---
+++ src/app/components/team.js
@@ -0,0 +1,76 @@
+export const divers = [
+  {
+    name: "Cecilia Conaco",
+    role: "MSI",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Patrick Cabaitan",
+    role: "MSI",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Marielle Ann Carungay",
+    role: "MSI",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Gabriella Juliane Maala",
+    role: "MSI",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Vanessa Joy Diamante",
+    role: "MSI",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Francis Gideon Tagnong",
+    role: "MSI",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+];
+
+export const heads = [
+  {
+    name: "Cecilia Conaco, PhD",
+    role: "Project Leader",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Vanessa Joy Diamante",
+    role: "Project Co-lead",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+];
+
+export const mentors = [
+  {
+    name: "Dan Jethro Masacupan",
+    role: "Bioinformatics Data Analyst",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Josh Gregor Dizon",
+    role: "HPC Director",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+];
+
+export const interns = [
+  {
+    name: "Elishia Janelle Ocson",
+    role: "Documentation and Quality Assurance",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Gliezel Ann Pajarilla",
+    role: "Backend Developer",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+  {
+    name: "Revien Rodriguez",
+    role: "Frontend Developer",
+    image: "/assets/about-us/PA103759.jpg",
+  },
+];
